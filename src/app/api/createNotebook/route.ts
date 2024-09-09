3e2e29b695f65831e910639be907237774fa5e7c
tc94 <tze.chong1994@gmail.com>
2024-09-09 10:48:02 +0800

    edge function

--- src/app/api/createNotebook/route.ts
+++ src/app/api/createNotebook/route.ts
@@ -6,6 +6,8 @@ import { generateImage, generateImagePrompt } from "@/lib/openai";
 import { auth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
+export const runtime = "edge";
+
 export async function POST(req: Request) {
   const { userId } = auth();
   if (!userId) {
@@ -41,7 +43,7 @@ export async function POST(req: Request) {
     .returning({
       insertedId: $notes.id,
     });
-    
+
   return NextResponse.json({
     note_id: note_ids[0].insertedId,
   });
